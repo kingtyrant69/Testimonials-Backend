9eefeaa9cae81640e7df130412e5dd5e1ee202fe
kingtyrant69 <aman.agarwal.1509@gmail.com>
2024-10-20 18:45:26 +0530

    other changes

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -1,3 +1,25 @@
 import {ApiError} from '../utils/ApiError.js'
 import {ApiResponse} from '../utils/ApiResponse.js'
 import {asyncHandler} from '../utils/asyncHandler.js'
+import jwt from 'jsonwebtoken'
+import { isValidObjectId } from 'mongoose'
+
+const getSpaces = asyncHandler(async(req,res)=>{
+    const user = req.user
+    if(!user){
+        throw new ApiError(400, "User Not Found");
+    }
+    if(!isValidObjectId(user?._id)){
+        throw new ApiError(400, "Invalid Object")
+    }
+    const spaces = await Space.find({
+        owner:user._id
+    })
+    if(spaces.length=== 0){
+        return res.status(200).json(new ApiResponse(200, {}, "User has no spaces"));
+    }
+    return res.status(200).json(new ApiResponse(200, spaces, "Spaces Fetched Successfully"))
+})
+
+
+export {getSpaces}
--- src/routes/dashboard.route.js
+++ src/routes/dashboard.route.js
@@ -1,9 +1,8 @@
 import {Router} from 'express'
 import { verifyJWT } from '../middlewares/auth.middleware.js'
-import {} from '../controllers/dashboard.controller.js'
+import {getSpaces} from '../controllers/dashboard.controller.js'
 
 const router = Router();
-
 
 
 export default router;
